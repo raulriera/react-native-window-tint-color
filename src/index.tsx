490541e7bed73f3b931a53be1977ebbc248344a7
Raul Riera <rieraraul@gmail.com>
2021-09-05 11:41:42 -0400

    Added more documentation

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import { NativeModules } from 'react-native';
 
 type WindowTintColorType = {
+  /**
+   * Sets the main window into color using the specified RGB color component values.
+   * @param red  The value of the red component, from 0 to 255.
+   * @param green  The value of the green component, from 0 to 255.
+   * @param blue  The value of the blue component, from 0 to 255.
+   * @returns Color generated in the native codebase.
+   */
   setTintColor(red: number, green: number, blue: number): Promise<number>;
 };
 
